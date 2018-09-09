7d003aa91da5e7bd584d954bf4781a34fad73e1b
Wake <georgerincon2@gmail.com>
2018-09-08 21:10:24 -0400

    wrote base obj constructor - still testing functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+
+
+
+
+var megaX = new fighter(150, 5, 20, "X");
+
 function fighter(health, attack, counter, name){
     this.health = health;
     this.attack = attack;
@@ -5,19 +11,50 @@ function fighter(health, attack, counter, name){
     this.name = name;
     this.timesAttacked = 0;
     this.isUser = false;
-    
+    this.isDead = false;
+    this.isDefending = false;
+    this.createElem = function () {
+        var newDiv = document.createElement("div");
+        var image = $("<img>", {
+            src : "../images/1stcharplaceholder.png"
+        });
+        var name = this.name;
+        var hp = this.health;
+
+        newDiv.append(name).append(image).append(hp);
+
+        $("#box1").html(newDiv);
+
+    }
+    this.moveElemToDef = function () {
+     
+        var newDiv = document.createElement("div");
+        var image = $("<img>", {
+            src : "../assets/images/1stcharplaceholder.png"
+        });
+        var name = this.name;
+        var hp = this.health;
+
+        $(newDiv).append(name).append(image).append(hp);
+
+        $("#defend-space").html(newDiv);
+
+    }
     this.incUserMulti = function (){
         this.timesAttacked += 5;
     }
     this.battlePower = function (){
-        if(this.name === "marsh" && (isUser)){
+        if(this.isUser){
             var userAttackPower = this.attack + (Math.floor(Math.random * 10) + this.timesAttacked);
             return userAttackPower;
         }
-
     }
-    
-
+    this.testLog = function (){
+        console.log(this.name);
+        console.log(this.health);
+        console.log(this.attack);
+        console.log(this.isUser);
+    }
 }
 
 function battleField (user, opponent){
@@ -25,3 +62,14 @@ function battleField (user, opponent){
     opponent.health += -user.battlePower();
     user.incUserMulti();
 }
+
+function render(){
+    megaX.createElem();
+    megaX.testLog();
+
+
+}
+
+$(document).ready(function() {
+    render();
+});
